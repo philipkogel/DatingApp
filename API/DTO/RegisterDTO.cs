a3ca90072df1a3c98bd9e8e3e7d9335912dd5f56
philipkogel <philipkogel@yahoo.pl>
2021-06-23 07:54:13 +0200

    Error handling, middleware,  interceptors

--- API/DTO/RegisterDTO.cs
+++ API/DTO/RegisterDTO.cs
@@ -7,6 +7,7 @@ namespace API.DTO
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -21,8 +21,6 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/members');
-    }, error => {
-      this.toastr.error(error.error);
     })
   }
 
